47b08bebe27baa7a35e354029cd18193cb0fb74f
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-03-15 12:07:17 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hannibal Lecter</title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+  <div class="main-frame">
+    <div class="picture">
+      <h1>Hannibal<br>Lecter</h1>
+      <button class="button-contact" id="submit" type="submit">Contactez-moi</button>
+    </div>
+  </div>
+
+  <div class="layer">
+    <h1>Critique Culinaire</h1>
+    <div class="plate"></div>
+    <div class="menu">
+      <h2>Menu</h2>
+      <div class="experience">
+        <h3>Amuse-bouches</h3>
+        <p>Expériences</p>
+      </div>
+      <div class="formation">
+        <h3>Entrée</h3>
+        <p>Formation</p>
+      </div>
+      <div class="talents">
+        <h3>Plat-principal</h3>
+        <p>Talents</p>
+      </div>
+      <div class="interet">
+        <h3>Fromage</h3>
+        <p>Intérêts</p>
+      </div>
+      <div class="coordonees">
+        <h3>Dessert</h3>
+        <p>Coordonnées</p>
+      </div>
+    </div>
+    <div class="social-media">
+
+      <a href=""><img src="" alt="Lien Instagram"></a>
+      <a href=""><img src="" alt="Lien Twitter"></a>
+      <a href=""><img src="" alt="Lien Linkedin"></a>
+
+    </div>
+
+  </div>
+
+
+</body>
+
+</html>
