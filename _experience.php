5b2ccffc158ba88656504c204b709130edb28a45
thomaslavalade <thomaslavalade@gmail.com>
2023-03-23 23:41:57 +0100

    création et finalisation modal Expérience

---
+++ _experience.php
@@ -0,0 +1,8 @@
+<li>
+      <div>
+            <time><?= $experience['date']; ?></time>
+            <span class="descriptionExperience">
+                  <?= $experience['description']; ?></span>
+            <span class="logoLecter"><img src="assets/images/butterfly.png" alt="logo Lecter"></span>
+      </div>
+</li>
