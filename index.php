e613f5344534d732e17e35e441bfde36d38901dc
thomaslavalade <thomaslavalade@gmail.com>
2023-03-23 21:42:26 +0100

    réalisation modal Coordonnées

--- index.php
+++ index.php
@@ -15,35 +15,35 @@ require 'formationData.php';
   <link rel="stylesheet" href="assets/css/formation.css" />
   <link rel="stylesheet" href="assets/css/interets.css">
   <link rel="stylesheet" href="/assets/css/talents.css">
+  <link rel="stylesheet" href="./assets/css/coordonnee.css">
   <link rel="icon" type="image/png" href="assets/images/couteau.png">
+  <script src="https://kit.fontawesome.com/63ffa44c37.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css2?family=Cormorant&family=Italianno&family=Libre+Franklin&display=swap" rel="stylesheet" />
 
 </head>
 
 <body>
   <div class="container">
-     <input type="checkbox" id="ring"/>
-    <div class="picture"> 
-    
-      <label for="ring"><img id="ring" src="/assets/images/sonnette.png" /></label>
-      
-        <h1>Hannibal<br>Lecter</h1> 
+    <input type="checkbox" id="ring" />
+    <div class="picture">
 
-         <section id="contact">
+      <label for="ring"><img id="ring" src="/assets/images/sonnette.png" /></label>
+
+      <h1>Hannibal<br>Lecter</h1>
+
+      <section id="contact">
 
         <a href="#more_contact">Contact</a>
-              <dialog id="more_contact">
-                <a class="close" href="#close">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
-                    viewBox="0 0 16 16">
-                    <path
-                      d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                  </svg>
-                </a>
-              </dialog>
-            </section>
+        <dialog id="more_contact">
+          <a class="close" href="#close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+          </a>
+        </dialog>
+      </section>
 
-  </div>
+    </div>
     <div class="layer">
       <h1>Critique Culinaire</h1>
       <div class="plate"></div>
@@ -133,22 +133,18 @@ require 'formationData.php';
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                   </svg>
                 </a>
-                <h1>Dessert</h1>
-                <h2>Coordonnées</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla facilis placeat nemo provident dolore
-                  repellat obcaecati harum enim deleniti distinctio ipsam modi tempora minus suscipit natus
-                  necessitatibus, fugiat quasi sit!</p>
+                <?php include('_coordonnees.php'); ?>
               </dialog>
             </section>
           </li>
-        </ul>   
-        <div class="logo"> <img src="/assets/images/butterfly.png" alt="Papillon"/></div>       
-            
+        </ul>
+        <div class="logo"> <img src="/assets/images/butterfly.png" alt="Papillon" /></div>
+
 
       </div>
-    <div class="plate">
-      <span class="inner_plate"></span>
-    </div>
+      <div class="plate">
+        <span class="inner_plate"></span>
+      </div>
 
     </div>
   </div>
